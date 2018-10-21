52d95b1c699f03dda0531592d33257a40683f410
Mark Tellez <mark.tellez@icloud.com>
2018-10-21 07:03:36 -0500

    added about us page and make landing exact

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/landing'
+import AboutUs from './pages/about-us'
 
 class App extends Component {
   render() {
     return <Switch>
-      <Route exact to={`/`} component={Landing} />
+      <Route exact path={`/`} component={Landing} />
+      <Route exact path={`/about`} component={AboutUs} />
     </Switch>
   }
 }
---
+++ src/pages/about-us/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class AboutUsPage extends React.Component {
+  render() {
+    return <div>
+      <h1>About Us page</h1>
+    </div>
+  }
+}
+
+export default AboutUsPage
