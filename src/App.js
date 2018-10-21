dac8d099b450771a34e00bc4101975928edba533
Mark Tellez <mark.tellez@icloud.com>
2018-10-21 07:01:56 -0500

    added switch, page, and route for landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Landing from './pages/landing'
 
 class App extends Component {
   render() {
-    return <div>
-
-    </div>
+    return <Switch>
+      <Route exact to={`/`} component={Landing} />
+    </Switch>
   }
 }
 
